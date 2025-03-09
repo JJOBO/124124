6cbfa3d7a8fa9ae40400a0d513ddcaaaa9000370
root <root@DESKTOP-KS7E5CV.>
2025-03-09 21:19:44 +0900

    test2

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './../pages/home/Home';
-import Community from './../pages/community/Community';
-import UsedMarket from './../pages/used-market/Index';
-import NotFound from '../pages/error/NotFound';
-import SignIn from './../pages/sign-in/SignIn';
-import Layout from './layout/Layout';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./../pages/home/Home";
+import UsedMarket from "./../pages/used-market/Index";
+import NotFound from "../pages/error/NotFound";
 
 function App() {
   return (
     <>
       <Layout>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/community' element={<Community />} />
-          <Route path='/used-market' element={<UsedMarket />} />
-          <Route path='/login' element={<SignIn />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/used-market" element={<UsedMarket />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
     </>
